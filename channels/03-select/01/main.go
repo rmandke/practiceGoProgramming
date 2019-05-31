072b01794e79e83eb5b237959b13e61442a905b8
Raj Mandke <raj@capiche.io>
2019-05-31 13:06:33 -0700

    Code refactured

--- channels/03-select/01/main.go
+++ channels/03-select/01/main.go
@@ -4,34 +4,33 @@ import "fmt"
 
 func main() {
 
-	eve := make(chan int)
+	even := make(chan int)
 	odd := make(chan int)
 	quit := make(chan int)
 
-	go send(eve, odd, quit)
-
-	receive(eve, odd, quit)
-
+	go send(even, odd, quit)
+	receive(even, odd, quit)
 	fmt.Println("about to exit")
-
 }
 
 func receive(even <-chan int, odd <-chan int, quit <-chan int) {
+
 	for {
 		select {
+
 		case v := <-even:
 			fmt.Println("from the even channel", v)
 		case v := <-odd:
-			fmt.Println("from the even channel", v)
+			fmt.Println("from the odd channel", v)
 		case v := <-quit:
 			fmt.Println("from the quit channel", v)
 			return
 		}
-
 	}
 }
 
 func send(even chan<- int, odd chan<- int, quit chan<- int) {
+
 	for i := 0; i < 100; i++ {
 		if (i % 2) == 0 {
 			even <- i
@@ -40,9 +39,7 @@ func send(even chan<- int, odd chan<- int, quit chan<- int) {
 			odd <- i
 		}
 	}
-
 	quit <- 0
 	close(even)
 	close(odd)
-
 }
